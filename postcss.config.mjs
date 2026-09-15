b9e619982b4cc3131434361eea07ae2c4dfd0f7a
Rafael <opinagov.oficial@gmail.com>
2026-09-15 02:16:03 -0300

    limpa marcadores de conflito do postcss

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,18 +1,2 @@
-<<<<<<< HEAD
-const config = {
-  plugins: {
-    "@tailwindcss/postcss": {},
-  },
-};
-
+const config = { plugins: { '@tailwindcss/postcss': {} } };
 export default config;
-=======
-/** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: {
-    '@tailwindcss/postcss': {},
-  },
-}
-
-export default config
->>>>>>> e71803c197cf8d7169ef621a0df403b6439187ad
